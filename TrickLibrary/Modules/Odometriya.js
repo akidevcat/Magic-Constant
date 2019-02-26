b74688ccffd8a4e1b0de0f187d1943481753cb3f
Alexander Rawyne <38999255+wolfniey@users.noreply.github.com>
2019-02-27 00:36:52 +0300

    Some fixes

--- TrickLibrary/Modules/Odometriya.js
+++ TrickLibrary/Modules/Odometriya.js
@@ -13,13 +13,14 @@ You can change 'deltat' to the loop delay value. For example:
 
 */
 //This code requiers:
-//eLeft, eRight, pi, cpr, d, l
+//+ eLeft, eRight, pi, cpr, d, l
 var odometriya = {}
 
 //These variables are based on the given task
 //If they're known then you should update them here:
 odometriya.x = 0
 odometriya.y = 0
+odometriya.distance = 0
 odometriya.teta = 0 //90* = pi / 2; 180* = pi; 270* = 1.5 * pi; 360* = 2 * pi
 //This is the delay between iterations inside the main loop (<Your MS Delay> / 1000)
 //If this value is 0 then it'll be calculated automatically (!but less accurate!)
@@ -62,6 +63,7 @@ odometriya.Update = function() {
 	deltateta = w * deltat;
 	this.x += Math.cos((2 * this.teta + deltateta) / 2) * v * deltat;
 	this.y += Math.sin((2 * this.teta + deltateta) / 2) * v * deltat;
+	this.distance += Math.abs(v * deltat);
 	this.teta += deltateta;
 	
 	//print("x: " + this.x); //<<<<<<< DEBUG TUUUT <<<<<<<<
