8bee1b817f65d90e30e0a2e8fb85ed64b004c071
Alexander Rawyne <38999255+wolfniey@users.noreply.github.com>
2019-03-03 22:59:12 +0300

    Create compose.py

---
+++ TrickLibrary/compose.py
@@ -0,0 +1,9 @@
+import glob, os
+
+result = ""
+for file in glob.glob("Modules/*.js"):
+    with open(file, 'r') as f:
+        data=f.read()
+        result += data + '\n' + '\n'
+with open("composed.js", "w") as tf:
+    tf.write(result)
