d25ce4ca5896fa5e8d6132b6d396516cbc4bc014
Alexander Rawyne <38999255+wolfniey@users.noreply.github.com>
2019-03-04 09:17:44 +0300

    Update 0_Header.js

--- TrickLibrary/Modules/0_Header.js
+++ TrickLibrary/Modules/0_Header.js
@@ -26,3 +26,25 @@ eRight.reset();
 //var tLeftSensor = brick.sensor(A4);//Касания
 //var lRightSensor = brick.sensor(A5);//Освещенности
 //var lLeftSensor = brick.sensor(A6);//Освещенности
+
+
+// Cell's parameters
+var cellLength = 52.5 * cpr / (pi * d);
+var length = 69;
+
+//Gyroscope settings
+var timeCalibrateRealRobot = 10000;
+var timeCalibrate2DRobot = 2000;
+// угловая скорость по x
+//brick.gyroscope().read()[0]; 
+// угловая скорость по y
+//brick.gyroscope().read()[1];
+// угловая скорость по z
+//brick.gyroscope().read()[2];
+//brick.gyroscope().read()[3]; // Valid time at gyroscope measurement
+// угол рысканья в миллиградусах −180 000 до 180 000
+//var az = brick.gyroscope().read()[6];
+// угол тангажа в миллиградусах −180 000 до 180 000
+//var ax = brick.gyroscope().read()[4];
+// угол крена в миллиградусах −90 000 до 90 000
+//var ay = brick.gyroscope().read()[5];
